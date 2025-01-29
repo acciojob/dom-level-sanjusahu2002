777c1466ed8338c6058546b6deb1dcde2492856e
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2025-01-29 10:42:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+        let counter = 0;
+        let element = document.getElementById('level');
+        while(element.parentNode){
+            counter++;
+            element = element.parentNode;
+            console.log(element);
+        }
+        alert("The level of the element is : "+counter);
